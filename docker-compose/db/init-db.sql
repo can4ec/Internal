65c37579bdb8b9cee904814c894eba09f6443032
can4ec@gmail.com <aler1207>
2025-01-15 23:17:53 +0300

    add docker compose and postgres script

---
+++ docker-compose/db/init-db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE test;
+
+create table public.users (
+    id bigint primary key,
+    name varchar(200)
+);
+
+insert into public.users (id, name)
+ select g.x, 'test'||g.x
+ from generate_series(1, 1000000) as g(x);
+
+----сбор статистики
+--vacuum analyze users;
+--
+--explain (analyze, costs off, buffers)
+--select * from users where name = 'test1';
